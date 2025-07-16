179d88ca7a576493210735fdc5a43e9503c05033
MohammadAli Arjomand <arjomand.dev@gmail.com>
2025-07-16 14:36:39 +0330

    port settings imrpoved

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config() 
+
 import express from 'express';
 
 // routes import
@@ -5,7 +8,7 @@ import authRoutes from './routes/auth.js'
 import recordRoutes from './routes/record.js'
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()) // Middleware to parse JSON bodies
 
